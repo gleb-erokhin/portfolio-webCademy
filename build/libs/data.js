faa36811584c05a906bf8cddcd5abfd49e754a58
gleb-erokhin <gleb.erokhin@gmail.com>
2024-11-08 07:50:12 +0300

    работа с карточками и кодом js

---
+++ build/libs/data.js
@@ -0,0 +1,44 @@
+const cardsData = `[
+    {
+        "id": 1,
+        "title": "ELLERY X M'&nbsp;O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": 52.00,
+        "img": "./img/item1.jpg"
+    },
+    {
+        "id": 2,
+        "title": "ELLERY X M'&nbsp;O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": 112.00,
+        "img": "./img/item2.jpg"
+    },
+    {
+        "id": 3,
+        "title": "ELLERY X M'&nbsp;O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": 222.00,
+        "img": "./img/item3.jpg"
+    },
+    {
+        "id": 4,
+        "title": "ELLERY X M'&nbsp;O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": 562.00,
+        "img": "./img/item4.jpg"
+    },
+    {
+        "id": 5,
+        "title": "ELLERY X M'&nbsp;O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": 62.00,
+        "img": "./img/item5.jpg"
+    },
+    {
+        "id": 6,
+        "title": "ELLERY X M'&nbsp;O CAPSULE",
+        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": 18.00,
+        "img": "./img/item6.jpg"
+    }
+]`
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import mobileNav from './modules/mobile-nav.js';
+import cardsAdd from './modules/main.js';
 mobileNav();
+cardsAdd();
