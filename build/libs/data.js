4ecde8415f8f50177a746b65429e11581fb307c0
gleb-erokhin <gleb.erokhin@gmail.com>
2024-11-09 00:18:46 +0300

    jS код карточек проектов. решение ошибок

--- build/libs/data.js
+++ build/libs/data.js
@@ -1,44 +1,56 @@
 const cardsData = `[
     {
         "id": 1,
-        "title": "ELLERY X M'&nbsp;O CAPSULE",
-        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
-        "price": 52.00,
-        "img": "./img/item1.jpg"
+        "title": "ЭпикТрэвел",
+        "info": "Верстка сайта визитки. 3 страницы, форма обратной связи, мобильная адаптация, retina ready.",
+        "pages": "<ul><li>Главная</li><li>Туры и направления</li><li>О компании</li><li>Контакты</li></ul>",
+        "skils": "<span>HTML,</span> <span>CSS,</span> <span>PHP</span>",
+        "links": "<span>Смотреть демо</span> <span>GitHub проекта</span>",
+        "img": "./img/cards/01.png"
     },
     {
         "id": 2,
-        "title": "ELLERY X M'&nbsp;O CAPSULE",
-        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
-        "price": 112.00,
-        "img": "./img/item2.jpg"
+        "title": "Сайт коворкинга",
+        "info": "Верстка лендинга для сайта коворкинга",
+        "pages": "",
+        "skils": "<span>HTML,</span> <span>SCSS,</span> <span>JS</span>",
+        "links": "<span>Смотреть демо</span> <span>GitHub проекта</span>",
+        "img": "./img/cards/02.png"
     },
     {
         "id": 3,
-        "title": "ELLERY X M'&nbsp;O CAPSULE",
-        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
-        "price": 222.00,
-        "img": "./img/item3.jpg"
+        "title": "Салон красоты",
+        "info": "Создание сайта салона красоты",
+        "pages": "",
+        "skils": "<span>HTML,</span> <span>SCSS,</span> <span>JS,</span> <span>Gulp</span>",
+        "links": "<span>Смотреть демо</span> <span>GitHub проекта</span>",
+        "img": "./img/cards/03.png"
     },
     {
         "id": 4,
-        "title": "ELLERY X M'&nbsp;O CAPSULE",
-        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
-        "price": 562.00,
-        "img": "./img/item4.jpg"
+        "title": "Квиз",
+        "info": "Онлайн опросник в формате квиза. Верстка и JS",
+        "pages": "",
+        "skils": "<span>HTML,</span> <span>SCSS,</span> <span>JS</span>",
+        "links": "<span>Смотреть демо</span> <span>GitHub проекта</span>",
+        "img": "./img/cards/04.png"
     },
     {
         "id": 5,
-        "title": "ELLERY X M'&nbsp;O CAPSULE",
-        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
-        "price": 62.00,
-        "img": "./img/item5.jpg"
+        "title": "CRM система",
+        "info": "Frontend для CRM системы",
+        "pages": "",
+        "skils": "<span>JS</span>",
+        "links": "<span>Смотреть демо</span> <span>GitHub проекта</span>",
+        "img": "./img/cards/05.png"
     },
     {
         "id": 6,
-        "title": "ELLERY X M'&nbsp;O CAPSULE",
-        "info": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
-        "price": 18.00,
-        "img": "./img/item6.jpg"
+        "title": "Каталог квартир",
+        "info": "Frontend для каталога недвижимости",
+        "pages": "",
+        "skils": "<span>JS,</span> <span>Webpack</span>",
+        "links": "<span>Смотреть демо</span> <span>GitHub проекта</span>",
+        "img": "./img/cards/02.png"
     }
 ]`
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,35 @@
-import mobileNav from './modules/mobile-nav.js';
-import cardsAdd from './modules/main.js';
-mobileNav();
-cardsAdd();
+// получаем данные из файла JSON
+const cards = JSON.parse(cardsData);
+
+// создаем переменную с поиском класса в который будем добавлять карточки
+const productsItems = document.querySelector('.portfolio__cards');
+console.log(productsItems);
+
+// вставляем карточки с данными из JSON
+cards.forEach(element => {
+    productsItems.insertAdjacentHTML('beforeend',
+        `
+            <div class="card" ${element.id}>
+                <a href="#!6">
+                    <div class="card__img-wrapper">
+                        <div class=" card__img">
+                            <img src="${element.img}" width="216" alt="">
+                        </div>
+                    </div>
+                    <div class="card__decs">
+                        <div class="card__header">
+                            <h3 class="title-3">${element.title}</h3>
+                        </div>
+                        <div class="card__text">
+                            <p class="text">${element.info}
+                            </p>
+                        </div>
+                        <div class="card__skils">
+                            ${element.skils}
+                        </div>
+                    </div>
+                </a>
+            </div> 
+        `
+    )
+});
